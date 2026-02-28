5803a6c1345262af4e6825dd88ee0d480206076c
hiba shereefa mk <hibashareefamk@gmail.com>
2026-02-28 16:29:40 +0530

    Add date formatting and current directory print

--- new_one.py
+++ new_one.py
@@ -1,3 +1,13 @@
+from datetime import datetime
+import os 
+
+today =datetime.now()
+formatted_data =today.strftime("%d-%m-%y")
+print(formatted_data)
+
+
+print(os.getcwd())
+
 num=[1,2,3,4,5,6,7]
 result =list(map(lambda x:x**3,num))
 print(result)
